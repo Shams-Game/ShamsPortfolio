fb26b448136fa60cef2b382f1e7638a85929db49
Shams <bazmi4u@gmail.com>
2025-12-26 18:43:57 +0530

    Fix mobile scroll by changing hero.jsx section id

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
   });
 
   return (
-    <section id="hero" className="relative overflow-hidden">
+    <section id="hero" className="relative md:overflow-hidden overflow-visible">
       <div className="absolute top-0 left-0 z-10">
         <img src="/images/bg.png" alt="" />
       </div>
